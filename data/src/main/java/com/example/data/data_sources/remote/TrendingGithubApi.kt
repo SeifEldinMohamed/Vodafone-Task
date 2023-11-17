e44d40cb421ef545a2227d6e2e5b1768de0ee4a4
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-11-17 21:18:22 +0200

    - adjust endpoint

--- data/src/main/java/com/example/data/data_sources/remote/TrendingGithubApi.kt
+++ data/src/main/java/com/example/data/data_sources/remote/TrendingGithubApi.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface TrendingGithubApi {
-    @GET("search/repositories?q=language=+sort:stars")
+    @GET("search/repositories?q=language")
     suspend fun fetchTrendingRepositories(): Response<TrendingGithubDataModel>
 }
